7741aad97bd95d75b16609e243927e47d4fa38ff
Bayen13 <reobre432@gmail.com>
2025-09-01 11:52:45 +0200

    Résolution du conflit dans PressingDetailPage.tsx

--- src/services/maps.ts
+++ src/services/maps.ts
@@ -37,7 +37,7 @@ export const getRoute = async (
     console.log('Fetching route from:', origin, 'to:', destination, 'mode:', mode);
     
     const response = await axios.get(
-      `http://localhost:5002/api/v1/maps/directions`,
+      `https://geopressci-akcdaadk.b4a.run/api/v1/maps/directions`,
       {
         params: {
           origin: `${origin.lat},${origin.lng}`,
